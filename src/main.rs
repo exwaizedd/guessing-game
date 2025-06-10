1a1f048bab69050b317699b5900a25bc04e33282
Exwaizedd <tegaowivri@gmail.com>
2025-06-11 00:07:47 +0100

    New Commit

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,64 @@ use std::io::{self, Write};
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main(){
-    println!("welcome to the Guessing game!!");
-    
-    let mut counter: i32 = 0; 
+fn main () {
+  let mut counter = 0;
 
-    loop {
-let guess = loop {
-    println!("Please enter your guess");
-    io::stdout().flush().unwrap();
-    let mut _guess = String::new();
-    io::stdin().read_line(&mut _guess).expect("Failed to read input");
-    match _guess.trim().parse::<i32>(){
-        Ok(temp ) => break temp,
+  loop {
+    let guess = loop {
+      println!("Guess the Secret Number");
+      io::stdout().flush().unwrap();
+
+      let mut _guess = String::new();
+      io::stdin().read_line(&mut _guess).expect("Please enter a valid Input");
+
+      match _guess.trim().parse::<i64>(){
+        Ok(temp) => break temp,
         Err(_) => {println!("Please enter a valid input");}
-    }
+      }
     
-};
+    };
 
-let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 1001);
+    match guess.cmp(&secret_number){
+      Ordering::Less => {println!("Too Low, Try Again");},
+      Ordering::Greater => {println!("Too High, Try Again")},
+      Ordering::Equal => {println!("You Win!!!"); break;}
+    }
 
-match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too Small"),
-    Ordering::Greater => println!("Too Big"),
-    Ordering::Equal => { println!("You Win!!!");
-                            break;                
-                                }
-
+    counter += 1;
+  }
+  println!("It took you {} attempts to guess the correct number", counter);
 }
 
-counter +=1;
 
-   }
+/*fn main() {
 
-   println!("{} Times", counter);
+  let mut counter = 0;
+  
+
+loop {
+    let guess = loop{
+      println!("Guess the Secret Number");
+      io::stdout().flush().unwrap();
+      let mut _guess = String::new();
+      io::stdin().read_line(&mut _guess).expect("Failed to read input");
+      match _guess.trim().parse::<i32>() {
+          Ok(temp) => break temp,
+          Err(_) => {println!("Please enter a valid input");}
+      }
+    };
+
+    let secret_number = rand::thread_rng().gen_range(1, 1001);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => {println!("Too Small, Try Again");},
+        Ordering::Greater => {println!("Too Big, Try Again");},
+        Ordering::Equal => {println!("You Win!!!");break;}
+    }
+    counter += 1;
+
 
 }
+println!("It took you {} guesses to get the Secret Number", counter);
+} */
