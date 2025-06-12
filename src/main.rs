5c88fa30c5de21c9344d1e4e37d42c551a60f138
Exwaizedd <tegaowivri@gmail.com>
2025-06-12 17:13:02 +0100

    Compound Data Types in Rust (Tuples, Arrays, Expressions, Statement, if-else

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main () {
-  let mut counter = 0;
+  /* let mut counter = 0;
 
   loop {
     let guess = loop {
@@ -30,8 +30,77 @@ fn main () {
 
     counter += 1;
   }
-  println!("It took you {} attempts to guess the correct number", counter);
+  println!("It took you {} attempts to guess the correct number", counter); */
+
+  let new_number: i32 = "31".parse().expect("Not a number");
+  println!("The number is {}", new_number);
+
+  let tup = (500, 6.4, 1);
+  //let (x, y, z) = tup;
+
+  println!("The value of x is: {}", tup.0);
+  println!("The value of y is: {}", tup.1);
+  println!("The value of z is: {}", tup.2);
+
+  let x = tup.0;
+  let y = tup.1;
+  let z = tup.2;
+
+  println!("The value of x is: {}", x);
+  println!("The value of y is: {}", y);
+  println!("The value of z is: {}", z);
+
+  let a_array = [1, 2, 3, 4, 5];
+
+  let months = ["January", "February", "March", "April",
+                                    "May", "June", "July", "August", "September", 
+                                    "October", "November", "December"];
+
+  println!("The first month of the year is {}", months[0]);
+let me = 5;
+let you = {let a = 3; a + 1};
+
+println!("The value of me is {}", me);
+println!("The value of you is {}", you);
+
+println!("{}", grade_converter('B'));
+
+let number = 6;
+
+if number % 2 == 0 {
+  println!("number is divisible by 2");
+} 
+else if number % 4 == 0 {
+  println!("number is divisible by 4");
 }
+else {
+  println!("The number is not divisible by 2 or 4");
+}
+
+let condition = true;
+let number = if condition {
+  5
+} else {
+  6
+};
+
+println!("The value of the number is: {}", number);
+
+}
+
+fn grade_converter(grade: char) -> f64 {
+  match grade {
+    'A' => 5.0,
+    'B' => 4.0,
+    'C' => 3.0,
+    'D' => 2.0,
+    'F' => 0.0,
+    _ => -1.0
+  }
+}
+
+
+
 
 
 /*fn main() {
